07744bb4c380b37eef5253b7239a489e31dcf21b
Kurt <klangrud@gmail.com>
2012-04-13 14:43:04 -0500

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: profile_d
+#
+# This module manages profile_d
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class profile_d {
+
+
+}
