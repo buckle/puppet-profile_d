aa7fa385f72a3849f770a356bcf7163c9e66ad1c
Kurt <klangrud@gmail.com>
2012-04-13 14:47:47 -0500

    Added manifest and template files for module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,10 @@
-# Class: profile_d
-#
-# This module manages profile_d
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-# [Remember: No empty lines between comments and class definition]
 class profile_d {
 
+  file {'/etc/profile.d':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
 
 }
